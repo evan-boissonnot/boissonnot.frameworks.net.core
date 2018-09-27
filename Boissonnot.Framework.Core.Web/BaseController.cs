93855a52c36f0c43cd4081fb5879286cc4ddf9e1
evan-boissonnot <commercial@boissonnot.Fr>
2018-09-27 22:48:12 +0200

    ajout bases controller

---
+++ Boissonnot.Framework.Core.Web/BaseController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boissonnot.Framework.Core.Web
+{
+    /// <summary>
+    /// Parente de tous les controllers
+    /// </summary>
+    public abstract class BaseController<TBusiness> : DefaultBaseController
+    {
+        #region Fields
+        private TBusiness _business = default(TBusiness);
+        #endregion
+
+        #region Constructors
+        public BaseController(DbContext context, TBusiness business) : base(context)
+        {
+            this._business = business;
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Accesseur du business par défaut
+        /// </summary>
+        protected TBusiness DefaultBusiness { get => this._business; }
+        #endregion
+    }
+}
---
+++ Boissonnot.Framework.Core.Web/DefaultBaseController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boissonnot.Framework.Core.Web
+{
+    /// <summary>
+    /// Parent par défaut des controllers
+    /// </summary>
+    public abstract class DefaultBaseController : Controller
+    {
+        #region Fields
+        private DbContext _context = null;
+        #endregion
+
+        #region Constructors
+        public DefaultBaseController(DbContext context)
+        {
+            this._context = context;
+        }
+        #endregion
+
+        #region Public methods
+        /// <summary>
+        /// Action par défaut
+        /// </summary>
+        /// <returns></returns>
+        public virtual IActionResult Index()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
