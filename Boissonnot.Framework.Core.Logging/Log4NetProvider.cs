ecdefd08c2ed24218d357c986ed584d73af032f8
evan-boissonnot <commercial@boissonnot.Fr>
2018-10-19 09:29:30 +0200

    ajout projet logging pour log4net

---
+++ Boissonnot.Framework.Core.Logging/Log4NetProvider.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace Boissonnot.Framework.Core.Logging
+{
+    /// <summary>
+    /// Provider pour fournir un logger
+    /// </summary>
+    public class Log4NetProvider : ILoggerProvider
+    {
+        #region Fields
+        private readonly string _log4NetConfigFile;
+        private readonly ConcurrentDictionary<string, Log4NetLogger> _loggers =
+            new ConcurrentDictionary<string, Log4NetLogger>();
+        #endregion
+
+        #region Constructors
+        public Log4NetProvider(string log4NetConfigFile)
+        {
+            this._log4NetConfigFile = log4NetConfigFile;
+        }
+        #endregion
+
+        #region Public methods
+        public ILogger CreateLogger(string categoryName)
+        {
+            return _loggers.GetOrAdd(categoryName, CreateLoggerImplementation);
+        }
+
+        public void Dispose()
+        {
+            this._loggers.Clear();
+        }
+        #endregion
+
+        #region Internal methods
+        private Log4NetLogger CreateLoggerImplementation(string name)
+        {
+            return new Log4NetLogger(name, Parselog4NetConfigFile(_log4NetConfigFile));
+        }
+
+        private static XmlElement Parselog4NetConfigFile(string filename)
+        {
+            XmlDocument log4netConfig = new XmlDocument();
+            log4netConfig.Load(File.OpenRead(filename));
+            return log4netConfig["log4net"];
+        }
+        #endregion
+    }
+}
---
+++ Boissonnot.Framework.Core.Logging/Log4netExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Boissonnot.Framework.Core.Logging
+{
+    /// <summary>
+    /// Extension pour le program de démarrage
+    /// </summary>
+    public static class Log4netExtensions
+    {
+        #region Public methods
+        /// <summary>
+        /// Ajoute le logger pour log4net
+        /// </summary>
+        /// <param name="factory"></param>
+        /// <param name="log4NetConfigFile"></param>
+        /// <returns></returns>
+        public static ILoggerFactory AddLog4Net(this ILoggerFactory factory, string log4NetConfigFile)
+        {
+            factory.AddProvider(new Log4NetProvider(log4NetConfigFile));
+            return factory;
+        }
+
+        /// <summary>
+        /// Ajoute le logger pour log4net
+        /// </summary>
+        /// <param name="factory"></param>
+        /// <returns></returns>
+        public static ILoggerFactory AddLog4Net(this ILoggerFactory factory)
+        {
+            factory.AddProvider(new Log4NetProvider("log4net.config"));
+            return factory;
+        }
+        #endregion
+    }
+}
